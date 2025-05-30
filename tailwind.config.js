120fa9ef1a1b9a0c3e33765ed871b4d99c807e01
Your Name <you@example.com>
2025-05-31 00:53:43 +0100

    fix: updated Tailwind config to apply styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{ts,tsx}',
-    './src/components/**/*.{ts,tsx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
