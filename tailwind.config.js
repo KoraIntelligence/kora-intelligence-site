eb4167db103f9fe748c343066f248bb4d54e1db8
KoraIntelligence <taimur@pathsunknown.org>
2025-05-31 01:54:38 +0100

    chore: ensure tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // ✅ This includes ALL components, pages, and styles
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
