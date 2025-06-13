a431d282513ae827a95ce2ce91ec5065da1720df
Your Name <you@example.com>
2025-06-13 19:41:35 +0100

    further fix

--- src/components/chat/CompanionChat.tsx
+++ src/components/chat/CompanionChat.tsx
@@ -126,8 +126,8 @@ export default function CompanionChat({ companionSlug, title, apiPath }: Compani
               {msg.sender === 'companion' && (
                 <span className="absolute top-1 right-2 opacity-5">
                   <img
-                    src={`public/assets/glyphs/glyph-${companionSlug}.png`}
-                    className="w-6 h-6"
+                    src={`/assets/glyphs/glyph-${companionSlug}.png`}
+                    className="w-6 h-6 animate-pulse opcity-5"
                     alt="glyph"
                   />
                 </span>
