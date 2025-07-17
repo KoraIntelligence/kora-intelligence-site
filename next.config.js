14f39f070ddefdcd8ecfba88ac2f687402255045
Your Name <you@example.com>
2025-07-18 00:27:55 +0100

    build error fix

--- next.config.js
+++ next.config.js
@@ -2,22 +2,15 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
-
-const nextConfig = withMDX({
-  pageExtensions: ['ts', 'tsx', 'mdx'],
-  experimental: {
-    largePageDataBytes: 256 * 1024, // 256kB limit
-  },
-});
-
 const { withContentlayer } = require('next-contentlayer');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    largePageDataBytes: 256 * 1024,
-  },
-};
+const nextConfig = withContentlayer(
+  withMDX({
+    pageExtensions: ['ts', 'tsx', 'mdx'],
+    experimental: {
+      largePageDataBytes: 256 * 1024, // 256kB limit
+    },
+  })
+);
 
-module.exports = withContentlayer(nextConfig);
 module.exports = nextConfig;
