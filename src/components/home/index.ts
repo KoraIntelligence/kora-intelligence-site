687bd5f375cc69aa247db86d555b06394c6d9e3d
Your Name <you@example.com>
2025-10-04 22:30:42 +0100

    move why kora above

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,7 +1,7 @@
 export { default as WhisperOfArrival } from './WhisperOfArrival';
+export { default as ComparisonTable } from './ComparisonTable';
 export { default as CompassFlame } from './CompassFlame';
 export { default as CompanionGrove } from './CompanionGrove';
-export { default as ComparisonTable } from './ComparisonTable';
 export { default as RitualEchoes } from './RitualEchoes';
 export { default as ZebraCovenant } from './ZebraCovenant';
 export { default as CallToPresence } from './CallToPresence';
